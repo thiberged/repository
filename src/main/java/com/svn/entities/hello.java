94f0a44181bf4917f4436ac64cb5e11b0df38bc7
thibergeD <damien.thiberge@gmail.com>
2018-05-31 10:09:54 +0200

    premier commit d'initialisation

---
+++ src/main/java/com/svn/entities/hello.java
@@ -0,0 +1,11 @@
+package main.java.com.svn.entities;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Coucou c'est moi");
+		System.out.println("Bonjour, c'est dieu");
+	}
+
+}
